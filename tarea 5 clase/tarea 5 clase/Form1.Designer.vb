5c76f4feefc7e323405ceb620cc6fd62d0ffeab7
alisonperezrosales <2556156630101@ingenieria.usac.edu.gt>
2024-03-19 23:45:58 -0600

    first commit

---
+++ tarea 5 clase/tarea 5 clase/Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        DataGridView1 = New DataGridView()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Label2 = New Label()
+        Label3 = New Label()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(291, 474)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(64, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Mensaje"
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(294, 21)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.Size = New Size(387, 360)
+        DataGridView1.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(37, 411)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 2
+        Button1.Text = "Insertar"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(109, 21)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 3
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(109, 96)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 4
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(515, 411)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 5
+        Button2.Text = "Cerrar"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(151, 411)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 6
+        Button3.Text = "Mostrar"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(279, 411)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(94, 29)
+        Button4.TabIndex = 7
+        Button4.Text = "Borrar"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(389, 411)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(94, 29)
+        Button5.TabIndex = 8
+        Button5.Text = "Actualizar"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(12, 28)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(58, 20)
+        Label2.TabIndex = 9
+        Label2.Text = "Codigo"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 103)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(87, 20)
+        Label3.TabIndex = 10
+        Label3.Text = "Descripción"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(775, 519)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Controls.Add(DataGridView1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+
+End Class
---
+++ tarea 5 clase/tarea 5 clase/Form1.vb
@@ -0,0 +1,97 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Dim con As New SqlConnection
+    Dim cmd As New SqlCommand
+    Dim i As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        con.ConnectionString = "Data Source=DESKTOP-V9FBPK8\SQLEXPRESS; Initial Catalog=linuxejemplo;integrated Security=True
+"
+        If con.State = ConnectionState.Open Then
+            con.Close()
+        End If
+        con.Open()
+        Label1.Text = "Guatemala tierra bendita por Dios"
+        Disp_data()
+    End Sub
+
+    Public Sub Disp_data()
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "select * from Categoria2"
+        cmd.ExecuteNonQuery()
+        Dim dt As New DataTable()
+        Dim da As New SqlDataAdapter(cmd)
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "insert into Categoria2 values('" & TextBox1.Text & "','" & TextBox2.Text & "')"
+        cmd.ExecuteNonQuery()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        Label1.Text = "Exito al grabar información."
+        Disp_data()
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        Try
+            If con.State = ConnectionState.Open Then
+                con.Close()
+            End If
+            con.Open()
+            i = Convert.ToInt32(DataGridView1.SelectedCells.Item(0).Value.ToString())
+            cmd = con.CreateCommand()
+            cmd.CommandType = CommandType.Text
+            cmd.CommandText = "select * from Categoria2 where Codigo=" & i&
+            cmd.ExecuteNonQuery()
+            Dim dt As New DataTable()
+            Dim da As New SqlDataAdapter(cmd)
+            da.Fill(dt)
+            Dim dr As SqlClient.SqlDataReader
+            dr = cmd.ExecuteReader(CommandBehavior.CloseConnection)
+            While dr.Read
+                TextBox1.Text = dr.GetString(1).ToString()
+                TextBox2.Text = dr.GetString(2).ToString()
+            End While
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Disp_data()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If con.State = ConnectionState.Open Then
+            con.Close()
+        End If
+        con.Open()
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "delete from Categoria2 where Codigo=" & TextBox1.Text
+        cmd.ExecuteNonQuery()
+        Disp_data()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If con.State = ConnectionState.Open Then
+            con.Close()
+        End If
+        con.Open()
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        Dim valorNumerico As Integer
+        valorNumerico = Val(TextBox1.Text)
+        Label1.Text = valorNumerico.ToString(ToString)
+        cmd.CommandText = "update Categoria2 set Codigo='" & TextBox1.Text & "', Descripcion='" & TextBox2.Text & "' where Codigo='" & i & "'"
+        cmd.ExecuteNonQuery()
+        Disp_data()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
